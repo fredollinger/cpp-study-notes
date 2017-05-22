5ce09cc905ed1641f0e88b0c90334c0474b35350
Frederick Ollinger <follinge@gmail.com>
2017-05-21 20:53:04 -0700

    Add pointer section

---
+++ code/pointers/address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int var = 1;
+int main()
+{
+   int num = 10;
+   printf("Value of var is: %d \n ", num);
+   printf("Address of var is: %p \n", &num);
+   return 0;
+}
---
+++ code/pointers/pointer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int var =10;
+    int *p;
+    p= &var;
+
+    printf ( "\n Address of var is: %p \n", &var);
+    printf ( "\n Address of var is: %p \n", p);
+
+    printf ( "\n Address of pointer p is: %p \n", &p);
+
+    /* Note I have used %p for p's value as it should be an address*/
+    printf( "\n Value of pointer p is: %p \n", p);
+
+    printf ( "\n Value of var is: %d \n", var);
+    printf ( "\n Value of var is: %d \n", *p);
+    printf ( "\n Value of var is: %d \n", *( &var));
+}
