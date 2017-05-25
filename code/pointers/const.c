bc444a2fea65e504921bba9cd9059c0693fb81c7
Frederick Ollinger <follinge@gmail.com>
2017-05-24 20:54:22 -0700

    Add passing pointers to functions

---
+++ code/pointers/const.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void swap (const int *pa, const int *pb) {
+    int tmp;
+    tmp = *pa;
+    *pa = *pb;
+    *pb = tmp;
+}
+
+int main()
+{
+   int a = 10;
+   int b = 20;
+
+   printf("before swap a: [%i] b: [%i] \n", a, b);
+
+   swap(&a, &b);
+
+   printf("after swap a: [%i] b: [%i] \n", a, b);
+
+   return 0;
+}
