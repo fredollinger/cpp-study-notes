e893a9bb3ae7967cef01060787fcdc10ba59cd4f
Frederick Ollinger <follinge@gmail.com>
2017-06-04 21:40:26 -0700

    STUB string struct

---
+++ code/pointers/string-struct.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // printf
+
+struct strstruct {
+    char **argv;
+    int argc;
+};
+
+int main()
+{
+    printf("TDB NOT DONE \n");
+    strstruct st;
+    st.argc = 0;
+    return 0;
+}
