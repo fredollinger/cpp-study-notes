d522bd4af0f5fe2d5d34220aba33735ab36811a7
Frederick Ollinger <follinge@gmail.com>
2017-06-15 07:48:22 -0700

    Add array of C strings

--- code/pointers/array-of-strings.c
+++ code/pointers/array-of-strings.c
@@ -1,6 +1,6 @@
 #include <stdio.h>  // printf
 #include <stdlib.h> // malloc
-#include <string.h> // malloc
+#include <string.h> // strncpy
 
 #define MAX_NUMBER_OF_STRINGS 10
 
@@ -9,16 +9,28 @@ struct strstruct {
     int argc;
 };
 
+void addstring(struct strstruct *st, const char *string) {
+    st->argv[st->argc] = (char*)malloc(strlen(string) + 1);
+    strncpy(st->argv[st->argc], string, strlen(string) + 1);
+    st->argc++;
+}
+
+void printstring(struct strstruct *st, int i) {
+    printf("print string [%i]: [%s] \n", i, st->argv[i]);
+}
+
 int main()
 {
     struct strstruct st;
     struct strstruct *p_st = &st;
     st.argc = 0;
 
-    const char *string0 = "My little pony";
-    st.argv[0] = (char*)malloc(strlen(string0) + 1);
-    strncpy(st.argv[0], string0, strlen(string0) + 1);
-    printf("string0: [%s] is [%s] \n", string0, st.argv[0]);
+    addstring(p_st, "applejack");
+    addstring(p_st, "pinkie pie");
+
+    printstring(p_st, 0);
+    printstring(p_st, 1);
+    printstring(p_st, 2);
 
     return 0;
 }
